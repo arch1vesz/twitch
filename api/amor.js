327281c0fa2b2b7dcfad14b3891e21179e64ef38
arch1vesz <kevinj_2015@outlook.com>
2025-04-09 14:39:51 -0600

    Update amor.js

--- api/amor.js
+++ api/amor.js
@@ -5,27 +5,27 @@ export default function handler(req, res) {
   const random = Math.floor(Math.random() * 3);
 
   const r25 = [
-    "Nomas? Mejor nadota eh.",
-    `Oye ${user}, pero si le quieres o no?`,
-    `Y mi celular tiene el ${numero + 1}% de batería.`
+    "Nomas? 👀 Mejor nadota eh.",
+    "Oye ${user}, pero si le quieres o no? 🤔",
+    "Y mi celular tiene el ${numero + 1}% de batería 🪫."
   ];
 
   const r = [
-    "Se tienen en sus corazones… ocupando espacio como una app que no uso pero no quiero borrar.",
-    "Con eso se aprende que el amor no es perfecto… pero se puede sobrevivir con un poco de sarcasmo y mucha paciencia.",
-    "Eso si es amor! Con intermitencias, lag y señal inestable."
+    "Se tienen en sus corazones 💘 ocupando espacio como una app que no uso pero no quiero borrar.",
+    "Con eso se aprende que el amor no es perfecto 💔 pero se puede sobrevivir con un poco de sarcasmo y mucha paciencia.",
+    "Eso si es amor! Con intermitencias, lag y señal inestable 📡."
   ];
 
   const r80 = [
-    "A poco si mucho amor? Nhaaaa.",
-    "Encontraron todo lo que buscaban... menos el control remoto, ese sigue perdido.",
-    "Oww, no se cambiarían por nada... o, tal vez por una Sub?"
+    "A poco si mucho amor? 👀 Nhaaaa.",
+    "Encontraron todo lo que buscaban... menos el control remoto, ese sigue perdido 🕵️‍♂️.",
+    "Oww, no se cambiarían por nada 💞 o, tal vez por una Sub? 🤑"
   ];
 
   let mensaje = `Hay un ${numero - 1}% de amor entre ${user} y ${touser}... `;
 
   if (numero === 0) {
-    mensaje += "Emmss... sin comentarios.";
+    mensaje += "Emmss... sin comentarios 👀.";
   } else if (numero <= 25) {
     mensaje += r25[random];
   } else if (numero < 80) {
