dd9d2e9ae6c9248d4e27004570c3c460f02bfe36
arch1vesz <kevinj_2015@outlook.com>
2025-04-27 01:02:49 -0600

    Update beso.js

--- api/beso.js
+++ api/beso.js
@@ -1,18 +1,18 @@
-// /api/beso.js
+er// /api/beso.js
 export default function handler(req, res) {
-  const { user = "Usuario", touser} = req.query;
+  const { sender = "Usuario", touser} = req.query;
 
  // Verificamos si 'touser' está vacío o no
-  if (!touser || touser.trim() === "" || user.toLowerCase() === touser.toLowerCase()) {
+  if (!touser || touser.trim() === "" || sender.toLowerCase() === touser.toLowerCase()) {
     // Si 'touser' está vacío, asignamos un valor por defecto
-    const msn = `Ey ${user}, no tires besos al aire, dedícaselo a alguien especial mencionándolo así "!beso @alguien" 😉`;
+    const msn = `Ey ${sender}, no tires besos al aire, dedícaselo a alguien especial mencionándolo así "!beso @alguien" 😉`;
     res.setHeader("Content-Type", "text/plain");
   res.status(200).send(msn);
   }else{
     const besos = [
-    `Un beso de parte de ${user} para ${touser}! 😘`,
-    `💖 ${user} le manda un besote a ${touser}! 😘`,
-    `💌 ${user} te ha dado un beso virtual, ${touser}`
+    `Un beso de parte de ${sender} para ${touser}! 😘`,
+    `💖 ${sender} le manda un besote a ${touser}! 😘`,
+    `💌 ${sender} te ha dado un beso virtual, ${touser}`
   ];
 
   const randomBeso = Math.floor(Math.random() * besos.length);
