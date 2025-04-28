7e0abc4a9d0e8e53e24f2838413d920fa463bc2a
arch1vesz <kevinj_2015@outlook.com>
2025-04-27 21:19:36 -0600

    Update so.js

--- api/so.js
+++ api/so.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
       return res.status(200).send(`¡${sender}, debes mencionar a alguien más para hacer un shoutout! 😉 Usa: !shoutout @usuario`);
     } else {
       try {
+        const cleanTouser = touser.replace('@', '');
         // Haciendo una solicitud GET a la API de Twitch
         const response = await fetch(`https://decapi.me/twitch/game/${touser}`);
   
@@ -26,9 +27,9 @@ export default async function handler(req, res) {
   
         // Armar el mensaje final
         const so = [
-          `Vayan a seguir a ${touser} 💖 en https://twitch.tv/${touser}, que trae contenido buenísimo con: ${game}.`,
-          `Vayan a seguir a ${touser} para que nos siga trayendo más contenido de ${game} en: https://twitch.tv/${touser} 💖`,
-          `Vayan y dejen su follow a ${touser} 💖 en https://twitch.tv/${touser}, que estaba transmitiendo: ${game}.`
+          `Vayan a seguir a ${touser} 💖 en https://twitch.tv/${cleanTouser}, que trae contenido buenísimo con: ${game}.`,
+          `Vayan a seguir a ${touser} para que nos siga trayendo más contenido de ${game} en: https://twitch.tv/${cleanTouser} 💖`,
+          `Vayan y dejen su follow a ${touser} 💖 en https://twitch.tv/${cleanTouser}, que estaba transmitiendo: ${game}.`
         ];
   
         const randomSo = Math.floor(Math.random() * so.length);
